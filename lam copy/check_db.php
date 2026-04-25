6ab76cec08cbfffd674b101c8ff2524ae088a34e
KRPCG7 <kowshiqueroy@gmail.com>
2026-04-26 01:57:00 +0600

    update

--- lam copy/check_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/bootstrap.php';
-$sql = "SHOW TABLES LIKE 'product_entries'";
-$stmt = db()->query($sql);
-if ($stmt->rowCount() > 0) {
-    echo "TABLE_EXISTS";
-} else {
-    echo "TABLE_NOT_FOUND";
-}
